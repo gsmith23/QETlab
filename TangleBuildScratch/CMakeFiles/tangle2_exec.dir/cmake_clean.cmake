0ae64dfbbf33a291aa8e27549d0b44ee7a9d8605
Chloe McGeorge <s1321516@strappi.ph.ed.ac.uk>
2018-02-10 11:20:02 +0000

    lyso

--- TangleBuildScratch/CMakeFiles/tangle2_exec.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/tangle2_exec"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/tangle2_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build-posi-unpolComp1/script1.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
-  
- for i in `seq 1 10`; do
-     ./tangle2
-     mv Tangle2.root sim0002000_$i.root 
- done
